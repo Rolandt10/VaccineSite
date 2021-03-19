43abb3e2d3c611aa89e8c1e90390c7e1f3dbd253
Roland Toth <troland703@gmail.com>
2021-03-19 23:49:16 +0100

    Endpoints added

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,51 @@
 module.exports = function(app) {
+    app.get('/', function(req, res, next) {
+
+    });
+
+    app.get('/information', function(req, res, next) {
+        
+    });
+
+    app.get('/vaccinationpoint', function(req, res, next) {
+        
+    });
+
+    app.use('/vaccinationpoint/new', function(req, res, next) {
+        
+    });
+
+    app.use('/vaccinationpoint/edit/:oltopontid', function(req, res, next) {
+        
+    });
+
+    app.get('/vaccinationpoint/delete/:oltopontid', function(req, res, next) {
+        
+    });
+
+    app.get('/vaccinationpoint/delete/:oltopontid', function(req, res, next) {
+        
+    });
+
+    app.use('/vaccine/:oltopontid/new', function(req, res, next) {
+        
+    });
+
+    app.use('/vaccine/:oltopontid/edit/:vakcinaid', function(req, res, next) {
+        
+    });
+
+    app.use('/vaccine/:oltopontid/decrease/:vakcinaid', function(req, res, next) {
+        
+    });
+
+    app.use('/vaccine/:oltopontid/increase/:vakcinaid', function(req, res, next) {
+        
+    });
+
+    app.get('/vaccine/:oltopontid/delete/:vakcinaid', function(req, res, next) {
+        
+    });
+
     
 }
