71f72c63380c08f5924fdf2b1d67825054d95080
Roland Toth <troland703@gmail.com>
2021-03-20 10:20:48 +0100

    Modified middlewares

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,17 @@
+const authenticationMW = require('../middleware/authenticationMW');
+const checkpassMW = require('../middleware/checkPassMW');
+const logoutMW = require('../middleware/logoutMW');
+const renderMW = require('../middleware/renderMW');
+const delVaccineMW = require('../middleware/vaccine/delVaccineMW');
+const getVaccineMW = require('../middleware/vaccine/getVaccineMW');
+const getVaccinesMW = require('../middleware/vaccine/getVaccinesMW');
+const saveVaccineMW = require('../middleware/vaccine/saveVaccineMW');
+const delVaccinePointMW = require('../middleware/vaccinationpoint/delVaccPointMW');
+const getVaccinePointMW = require('../middleware/vaccinationpoint/getVaccPointMW');
+const getVaccinePointsMW = require('../middleware/vaccinationpoint/getVaccPointsMW');
+const saveVaccinePointMW = require('../middleware/vaccinationpoint/saveVaccinePointMW');
+
+
 module.exports = function(app) {
     app.get('/', function(req, res, next) {
 
