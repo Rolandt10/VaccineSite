5d6aeefffe29962938dd0b32210f6827800077c4
Roland Toth <troland703@gmail.com>
2021-05-13 14:15:09 +0200

    Vacine is deleted if decreased to 0

--- middleware/vaccine/decreaseVaccineNumberMW.js
+++ middleware/vaccine/decreaseVaccineNumberMW.js
@@ -6,7 +6,21 @@ module.exports = function(objectRep) {
             return next();
         }
 
+        if(req.body.darabszam > res.locals.vakcina.darabszam) {
+            return next();
+        }
+
         res.locals.vakcina.darabszam -= req.body.darabszam;
+
+        //Ha az összes vakcina elfogyott, automatikusan törlésre kerül
+        if(res.locals.vakcina.darabszam == 0) {
+            res.locals.vakcina.remove(err => {
+                if (err) {
+                    return next(err);
+                }
+                return res.redirect(`/vaccine/${res.locals.oltopont._id}`);
+            });
+        }
 
         res.locals.vakcina.save(err => {
             if (err) {
